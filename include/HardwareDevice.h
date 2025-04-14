92ff4d5ea9930b2ee9dd67a336e9b441adb07e82
Lucas Chen <lucaschentao@gmail.com>
2025-04-14 10:42:47 -0500

    Add the Hardware Device generate sample data

---
+++ include/HardwareDevice.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "SensorSample.h"
+#include "SamplePacket.h"
+#include "uuidUtil.h"
+#include "Client.h"
+#include "AlgorithmInstance.h"
+
+namespace Hardware
+{
+    class HardwareDevice
+    {
+    public:
+        HardwareDevice(int magnetometers, int accelerometers, int gyroscopes, int interval_ms);
+        ~HardwareDevice();
+
+    public:
+        void register_client(Consumer::Client *client);
+        void unregister_client(Consumer::Client *client);
+
+    private:
+        void run();
+        Models::SensorSample random_sample();
+
+    private:
+        std::string uuid_;
+        int magnetometers_, accelerometers_, gyroscopes_;
+        int interval_ms_;
+        std::vector<Algo::AlgorithmInstance> algorithms_;
+        std::thread thread_;
+        std::atomic<bool> stop_;
+        std::vector<Consumer::Client *> clients_;
+        std::mutex clients_mutex_;
+    };
+}
---
+++ src/HardwareDevice.cpp
@@ -0,0 +1,78 @@
+#include "HardwareDevice.h"
+#include "Client.h"
+
+namespace Hardware
+{
+    HardwareDevice::HardwareDevice(int magnetometers, int accelerometers, int gyroscopes, int interval_ms)
+        : uuid_(std::move(Utils::generate_uuid()))
+        , magnetometers_(magnetometers)
+        , accelerometers_(accelerometers)
+        , gyroscopes_(gyroscopes)
+        , interval_ms_(interval_ms)
+        , stop_(false)
+    {
+        for (int i = 0; i < magnetometers_; ++i)
+        {
+            algorithms_.emplace_back(/* provide necessary constructor arguments here */);
+        }
+        thread_ = std::thread([this]()
+                                { this->run(); });
+    }
+
+    HardwareDevice::~HardwareDevice()
+    {
+        stop_ = true;
+        if (thread_.joinable())
+            thread_.join();
+    }
+
+    void HardwareDevice::register_client(Consumer::Client *client)
+    {
+        std::unique_lock<std::mutex> lock(clients_mutex_);
+        clients_.push_back(client);
+    }
+
+    void HardwareDevice::unregister_client(Consumer::Client *client)
+    {
+        std::unique_lock<std::mutex> lock(clients_mutex_);
+        clients_.erase(std::remove(clients_.begin(), clients_.end(), client), clients_.end());
+    }
+
+
+    void HardwareDevice::run()
+    {
+        while (!stop_)
+        {
+            Models::SamplePacket packet;
+            packet.uuid = uuid_;
+            packet.timestamp = std::chrono::steady_clock::now();
+            // generate samples
+            for (int i = 0; i < magnetometers_; ++i)
+                packet.magnetometers.push_back(random_sample());
+            for (int i = 0; i < accelerometers_; ++i)
+                packet.accelerometers.push_back(random_sample());
+            for (int i = 0; i < gyroscopes_; ++i)
+                packet.gyroscopes.push_back(random_sample());
+            // process with algorithms
+            for (int i = 0; i < magnetometers_; ++i)
+            {
+                std::vector<float> pose;
+                algorithms_[i].process(packet.magnetometers[i], pose, interval_ms_);
+                packet.poses.push_back(pose);
+            }
+            // distribute to clients
+            std::unique_lock<std::mutex> lock(clients_mutex_);
+            for (auto *client : clients_)
+            {
+                client->receive(packet);
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(interval_ms_));
+        }
+    }
+
+    Models::SensorSample HardwareDevice::random_sample()
+    {
+        return {(float)(rand() % 100) / 10.0f, (float)(rand() % 100) / 10.0f, (float)(rand() % 100) / 10.0f};
+    }
+} // namespace Hardware
+
