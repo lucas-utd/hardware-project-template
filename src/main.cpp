76151b31b9f81d82f9998f22be867ebbfc5fcb67
Lucas Chen <chen@spectralmd.com>
2025-04-14 11:13:11 -0500

    Use the new way to get randon interge to avoid warnings on MSVC

--- src/main.cpp
+++ src/main.cpp
@@ -15,19 +15,23 @@ using namespace std::chrono_literals;
 
 int main()
 {
-	srand(time(NULL));
+	// Initialize random number generator
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dist(1, 20);
+
+	// Create a vector of unique pointers to HardwareDevice and Client objects
 	std::vector<std::unique_ptr<Hardware::HardwareDevice>> devices;
 	std::vector<std::unique_ptr<Consumer::Client>> clients;
 
 	for (int i = 0; i < 10; ++i)
 	{
-		int m = 1 + rand() % 5;
-		int a = rand() % (m + 1);
-		int g = rand() % (a + 1);
-		int interval = (i % 3 == 0) ? 1 : ((i % 3 == 1) ? 2 : 3);
-		interval = (interval == 1) ? 1 : (interval == 2) ? 0.5
-														 : 0.33;
-		devices.push_back(std::make_unique<Hardware::HardwareDevice>(m, a, g, (int)(interval * 1000)));
+		int m = 1 + dist(gen) % 5;
+		int a = dist(gen) % (m + 1);
+		int g = dist(gen) % (a + 1);
+		int value = (i % 3 == 0) ? 1 : ((i % 3 == 1) ? 2 : 3);
+		int interval = (value == 1) ? 1000 : (value == 2) ? 500 : 330;
+		devices.push_back(std::make_unique<Hardware::HardwareDevice>(m, a, g, interval));
 	}
 
 	for (int i = 0; i < 5; ++i)
@@ -40,7 +44,7 @@ int main()
 	{
 		for (int j = 0; j < 3; ++j)
 		{
-			int idx = rand() % devices.size();
+			int idx = dist(gen) % devices.size();
 			devices[idx]->register_client(client.get());
 		}
 	}
